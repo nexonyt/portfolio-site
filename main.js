833f1d30cc690734ae210a1bb66ae8503bdbc733
Marcin <kaczmarekm090@gmail.com>
2023-03-12 23:14:02 +0100

    added new photos and features

--- main.js
+++ main.js
@@ -54,3 +54,22 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { 
     if(textArray.length) setTimeout(type, 100);
   });
+
+
+//SHADOW HEADER 
+
+const header = document.querySelector('header');
+
+const stickHeader = () => {
+  const scroll = window.scrollY;
+
+  if (scroll > 0) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
+
+window.addEventListener('scroll',stickHeader)
+
+
