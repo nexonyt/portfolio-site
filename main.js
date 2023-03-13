4647bb8b7896bc9f9ddc0114bd4acc72b457c5df
Marcin <kaczmarekm090@gmail.com>
2023-03-13 22:50:15 +0100

    added new features

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 const burgerButton = document.querySelector(".burger");
 const headerListUL = document.querySelector(".header-list");
-var counter=0;
+var counter = 0;
 
-burgerButton.addEventListener('click',() => {
-    if(counter%2==0) {
+burgerButton.addEventListener('click', () => {
+    if (counter % 2 == 0) {
         headerListUL.classList.add("active");
         burgerButton.classList.add("active");
         counter++;
@@ -16,44 +16,61 @@ burgerButton.addEventListener('click',() => {
     }
 });
 
+//MOVING SCROLL
+
+const menuLinks = document.querySelectorAll(".list-li");
+menuLinks.forEach(link => link.addEventListener('click', (e) => {
+    const key = e.target.dataset.key;
+
+    const section =
+        document.querySelector(`.${key}`).getBoundingClientRect().top +
+        window.pageYOffset - 180;
+
+    window.scrollTo({ top: section, behavior: 'smooth' });
+    
+    headerListUL.classList.remove("active");
+    burgerButton.classList.remove("active");
+}))
+
+
 
 
 
 // WRITING TEXT
 
 const changingTextSpan = document.querySelector("#changing-text");
-const textArray = ['Frontend Developer','Designer','Video Editor'];
+const textArray = ['Frontend Developer', 'Designer', 'Video Editor'];
 
 let arrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-    if(charIndex<textArray[arrayIndex].length) {
-        changingTextSpan.textContent+=textArray[arrayIndex].charAt(charIndex);
+    if (charIndex < textArray[arrayIndex].length) {
+        changingTextSpan.textContent += textArray[arrayIndex].charAt(charIndex);
         charIndex++;
-        setTimeout(type,100);
+        setTimeout(type, 100);
     }
     else {
-        setTimeout(erase,1000);
+        setTimeout(erase, 1000);
     }
 }
 
 function erase() {
-    if(charIndex>0) {
-        changingTextSpan.textContent=textArray[arrayIndex].substring(0,charIndex-1);
+    if (charIndex > 0) {
+        changingTextSpan.textContent = textArray[arrayIndex].substring(0, charIndex - 1);
         charIndex--;
-        setTimeout(erase,100);
+        setTimeout(erase, 100);
     }
     else {
         arrayIndex++;
-        if(arrayIndex>=textArray.length) arrayIndex=0;
-        setTimeout(type,1000)
+        if (arrayIndex >= textArray.length) arrayIndex = 0;
+        setTimeout(type, 1000)
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() { 
-    if(textArray.length) setTimeout(type, 100);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    if (textArray.length) setTimeout(type, 100);
+});
 
 
 //SHADOW HEADER 
@@ -61,15 +78,15 @@ document.addEventListener("DOMContentLoaded", function() {
 const header = document.querySelector('header');
 
 const stickHeader = () => {
-  const scroll = window.scrollY;
+    const scroll = window.scrollY;
 
-  if (scroll > 0) {
-    header.classList.add('active');
-  } else {
-    header.classList.remove('active');
-  }
+    if (scroll > 0) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
 };
 
-window.addEventListener('scroll',stickHeader)
+window.addEventListener('scroll', stickHeader)
 
 
